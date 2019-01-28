844bd45055138c4455522f5c24a637322a9e9924
Sebastian Makinson <smakinson@pairin.com>
2019-01-28 07:05:11 -0700

    Init

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/base32"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var (
+	envIsLoaded = false
+)
+
+// GenRandomBase32Bytes generates random number of bytes in base32 format
+func GenRandomBase32Bytes(num int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	token := make([]byte, num)
+	rand.Read(token)
+
+	return base32.StdEncoding.EncodeToString(token)
+}
+
+// GetEnvVariable get environment variable with default
+func GetEnvVariable(env string, d string) string {
+	if !envIsLoaded {
+		envIsLoaded = true
+		err := godotenv.Load()
+		if err != nil {
+			log.Info("Error loading .env file: " + err.Error())
+		}
+	}
+
+	val, exist := os.LookupEnv(env)
+
+	if !exist {
+		return d
+	}
+
+	return val
+}
