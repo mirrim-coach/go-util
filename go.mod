ee68b0b8141e2fe3e0558e03ae6b252ea1e2ed22
Sebastian Makinson <smakinson@pairin.com>
2019-05-10 13:43:31 -0600

    Added modules files and some better logging to secrets management

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/Pairin/go-util
+
+go 1.12
+
+require (
+	github.com/alicebob/gopher-json v0.0.0-20180125190556-5a6b3ba71ee6 // indirect
+	github.com/alicebob/miniredis v2.5.0+incompatible
+	github.com/aws/aws-sdk-go v1.19.28
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.8
+	github.com/joho/godotenv v1.3.0
+	github.com/selvatico/go-mocket v1.0.7
+	github.com/sirupsen/logrus v1.4.1
+	github.com/yuin/gopher-lua v0.0.0-20190206043414-8bfc7677f583 // indirect
+)
--- secrets.go
+++ secrets.go
@@ -51,28 +51,28 @@ func getSecret(secretEnv string, secretName string) {
 			switch aerr.Code() {
 			case secretsmanager.ErrCodeDecryptionFailure:
 				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+				Logger().Error(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
 
 			case secretsmanager.ErrCodeInternalServiceError:
 				// An error occurred on the server side.
-				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+				Logger().Error(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
 
 			case secretsmanager.ErrCodeInvalidParameterException:
 				// You provided an invalid value for a parameter.
-				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+				Logger().Error(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
 
 			case secretsmanager.ErrCodeInvalidRequestException:
 				// You provided a parameter value that is not valid for the current state of the resource.
-				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+				Logger().Error(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
 
 			case secretsmanager.ErrCodeResourceNotFoundException:
 				// We can't find the resource that you asked for.
-				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+				Logger().Error(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			fmt.Println(err.Error())
+			Logger().Error(err.Error())
 		}
 		return
 	}
@@ -86,7 +86,7 @@ func getSecret(secretEnv string, secretName string) {
 		decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
 		len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
 		if err != nil {
-			fmt.Println("Base64 Decode Error:", err)
+			Logger().Error("Base64 Decode Error:", err)
 			return
 		}
 		secretString = string(decodedBinarySecretBytes[:len])
@@ -99,8 +99,11 @@ func getSecret(secretEnv string, secretName string) {
 		panic(err)
 	}
 
+	Logger().Infof("Setting ENV Variables from %s", secretEnv)
+
 	for k, v := range variables {
 		k = convertKey(secretEnv, k)
+		Logger().Infof("- Setting %s", k)
 		os.Setenv(k, fmt.Sprintf("%v", v))
 	}
 
